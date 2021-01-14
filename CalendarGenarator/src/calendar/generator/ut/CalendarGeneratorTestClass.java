a86280d816b6f67e06ba5942ea2279154e314f31
AMPC0sta <antonio.manuel.pereira.costa@gmail.com>
2021-01-14 22:31:54 +0000

    20210114-2332 Adding a testing class!

---
+++ CalendarGenarator/src/calendar/generator/ut/CalendarGeneratorTestClass.java
@@ -0,0 +1,11 @@
+package calendar.generator.ut;
+
+public class CalendarGeneratorTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("******************** Testing class ************************");
+	}
+
+}
